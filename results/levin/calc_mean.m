73e3f845fab8f20b0b901eb2699c784692a7340f
Nagy Kristof <nagykristof958@gmail.com>
2024-05-07 11:26:18 +0200

    Update evaluation

--- results/levin/calc_mean.m
+++ results/levin/calc_mean.m
@@ -1,73 +1,79 @@
-name = "ypred";
+name = "basic";
 maxshift = 5;
 
-mean_im_one_kernel = zeros(1, 8);
+mean_im_one_kernel = zeros(8, 2);
 mean_kernel_all_im = zeros(1, 8);
 for k = 1:8
     mean_kssim = zeros(1,4);
-    mean_xssim = zeros(1,4);
+    mean_xssim = zeros(4,2);
     for x = 1:4
-        tk = imread(name+"/im"+x+"_kernel"+k+"_img_k.png");
-        k2 = imread("gt/kernel"+k+".png");
-        [r1, c1] = size(tk);
-        [r2, c2] = size(k2);
+        % tk = imread(name+"/im"+x+"_kernel"+k+"_img_k.png");
+        % k2 = imread("gt/kernel"+k+".png");
+        % [r1, c1] = size(tk);
+        % [r2, c2] = size(k2);
         %tk = padarray(tk, [floor((r2-r1)/2), floor((c2-c1)/2)]);
         %tk = im2double(tk);
         %k2 = im2double(k2);
         %[p, s, e] = comp_upto_shift(tk, k2, maxshift);
         %mean_kssim(x) = s;%ssim(tk, k2);
 
-        tx = imread(name+"/im"+x+"_kernel"+k+"_img_x_cg.png");
+        % tx = imread("blur/im"+x+"_kernel"+k+"_img.png");
+        % tx = imread("selfdeblur/im"+x+"_kernel"+k+"_img_x.png");
+        % x2 = imread("gt/im"+x+".png");
+        % tx = im2double(tx);
+        % x2 = im2double(x2);
+        % [p, s, e] = comp_upto_shift(tx, x2, maxshift);
+        % mean_xssim(x, 1) = s;
+        
+        tx = imread(name+"/im"+x+"_kernel"+k+"_img_x.png");
         x2 = imread("gt/im"+x+".png");
-        [r2, c2] = size(tx);
-        [r1, c1] = size(x2);
-        tx = tx(floor((r2-r1)/2)+1:end-floor((r2-r1)/2), floor((c2-c1)/2)+1:end-floor((c2-c1)/2));
         tx = im2double(tx);
         x2 = im2double(x2);
         [p, s, e] = comp_upto_shift(tx, x2, maxshift);
-        mean_xssim(x) = s;%ssim(tx, x2);
+        mean_xssim(x, 2) = s;
         %figure, imshow([tx, x2])
     end
     mean_kernel_all_im(k) = mean(mean_kssim);
-    mean_im_one_kernel(k) = mean(mean_xssim);
+    mean_im_one_kernel(k, :) = mean(mean_xssim, 1);
 end
 mean(mean_im_one_kernel)
 mean(mean_kernel_all_im)
 
-mean_im_all_kernel = zeros(1,4);
-mean_kernel_one_im = zeros(1,4);
-for x = 1:4
-    mean_xssim = zeros(1,8);
-    mean_kssim = zeros(1,8);
-    for k = 1:8
-        tk = imread(name+"/im"+x+"_kernel"+k+"_img_k.png");
-        k2 = imread("gt/kernel"+k+".png");
-        [r1, c1] = size(tk);
-        [r2, c2] = size(k2);
-        tk = padarray(tk, [floor((r2-r1)/2), floor((c2-c1)/2)]);
-        %mean_kssim(k) = ssim(tk, k2);
-
-        tx = imread(name+"/im"+x+"_kernel"+k+"_img_x_cg.png");
-        x2 = imread("gt/im"+x+".png");
-        [r2, c2] = size(tx);
-        [r1, c1] = size(x2);
-        tx = tx(floor((r2-r1)/2)+1:end-floor((r2-r1)/2), floor((c2-c1)/2)+1:end-floor((c2-c1)/2));
-        tx = im2double(tx);
-        x2 = im2double(x2);
-        [p, s, e] = comp_upto_shift(tx, x2, maxshift);
-        mean_xssim(k) = s;%ssim(tx, x2);
-        %mean_xssim(k) = ssim(tx, x2);
-    end
-    mean_kernel_one_im(x) = mean(mean_kssim);
-    mean_im_all_kernel(x) = mean(mean_xssim);
-end
-mean(mean_im_all_kernel)
-mean(mean_kernel_one_im)
+% mean_im_all_kernel = zeros(1,4);
+% mean_kernel_one_im = zeros(1,4);
+% for x = 1:4
+%     mean_xssim = zeros(1,8);
+%     mean_kssim = zeros(1,8);
+%     for k = 1:8
+%         tk = imread(name+"/im"+x+"_kernel"+k+"_img_k.png");
+%         k2 = imread("gt/kernel"+k+".png");
+%         [r1, c1] = size(tk);
+%         [r2, c2] = size(k2);
+%         tk = padarray(tk, [floor((r2-r1)/2), floor((c2-c1)/2)]);
+%         %mean_kssim(k) = ssim(tk, k2);
+% 
+%         tx = imread(name+"/im"+x+"_kernel"+k+"_img_x_cg.png");
+%         x2 = imread("gt/im"+x+".png");
+%         [r2, c2] = size(tx);
+%         [r1, c1] = size(x2);
+%         tx = tx(floor((r2-r1)/2)+1:end-floor((r2-r1)/2), floor((c2-c1)/2)+1:end-floor((c2-c1)/2));
+%         tx = im2double(tx);
+%         x2 = im2double(x2);
+%         [p, s, e] = comp_upto_shift(tx, x2, maxshift);
+%         mean_xssim(k) = s;%ssim(tx, x2);
+%         %mean_xssim(k) = ssim(tx, x2);
+%     end
+%     mean_kernel_one_im(x) = mean(mean_kssim);
+%     mean_im_all_kernel(x) = mean(mean_xssim);
+% end
+% mean(mean_im_all_kernel)
+% mean(mean_kernel_one_im)
 
 % how good is image estimate for a given kernel?
-figure
-bar(mean_im_one_kernel)
-title("Image given kernel")
+% figure
+% bar(mean_im_one_kernel)
+% legend(["SelfDeblur", "Saját"])
+% title("Kernelek szerinti átlagos kép SSIM értékek")
 
 % how good is image estimate for all kernels?
 % figure
