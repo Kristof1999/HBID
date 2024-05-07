b876821f248f9e363985a7c71dadb7d54524e46c
Nagy Kristof <nagykristof958@gmail.com>
2024-05-07 10:50:58 +0200

    Added runners

--- experiment.py
+++ experiment.py
@@ -1,5 +1,9 @@
 from variants.multires import run_multires
 from variants.basic import run_basic
+from variants.mlp import run_mlp
+from variants.cnn import run_cnn
 
+#run_mlp()
+#run_cnn()
 #run_multires()
 run_basic()
